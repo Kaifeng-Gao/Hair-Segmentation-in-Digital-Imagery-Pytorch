397df9ad8a5cba69c4fd39630546a95e830010e9
Kai Gao <87157271+Kaifeng-Gao@users.noreply.github.com>
2023-12-09 16:09:38 -0500

    Fix bugs in labeling

--- datasets/hair.py
+++ datasets/hair.py
@@ -10,17 +10,16 @@ from torchvision.datasets.utils import download_url, check_integrity
 
 
 class FigaroDataset(Dataset):
-
     cmap = np.array([
-                        [0, 0, 0],        # background
-                        [128, 0, 0],      # straight
-                        [0, 128, 0],      # wavy
-                        [128, 128, 0],    # curly
-                        [0, 0, 128],      # kinky
-                        [128, 0, 128],    # braids
-                        [0, 128, 128],    # dreadlocks
-                        [128, 128, 128],  # short-men
-                    ], dtype=np.uint8)
+        [0, 0, 0],  # background
+        [128, 0, 0],  # straight
+        [0, 128, 0],  # wavy
+        [128, 128, 0],  # curly
+        [0, 0, 128],  # kinky
+        [128, 0, 128],  # braids
+        [0, 128, 128],  # dreadlocks
+        [128, 128, 128],  # short-men
+    ], dtype=np.uint8)
 
     def __init__(self, root_dir, train=True, download=False, joint_transforms=None,
                  image_transforms=None, mask_transforms=None, gray_image=False):
@@ -52,21 +51,20 @@ class FigaroDataset(Dataset):
         self.mask_transforms = mask_transforms
         self.gray_image = gray_image
 
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
         img_path = self.img_path_list[idx]
         img = Image.open(img_path)
 
         mask_path = self.mask_path_list[idx]
         mask = Image.open(mask_path)
-
-        class_label = self.get_class_label(os.path.basename(img_path))
+        print(mask)
+        class_label = self.get_class_label(os.path.basename(mask_path))
         # 将mask转换为numpy数组
-        mask_array = np.array(mask)
+        mask_array = np.array(mask, dtype=np.uint8)
         # 创建一个新的mask，其初始值全为0
         class_mask = np.zeros_like(mask_array)
         # 将对应于原始mask的非零部分的位置设置为类别值
         class_mask[mask_array > 0] = class_label
-
         if self.joint_transforms is not None:
             img, class_mask = self.joint_transforms(img, class_mask)
 
@@ -84,7 +82,7 @@ class FigaroDataset(Dataset):
 
         if self.gray_image:
             gray = img.convert('L')
-            gray = np.array(gray, dtype=np.float32)[np.newaxis, ] / 255
+            gray = np.array(gray, dtype=np.float32)[np.newaxis,] / 255
             return img, class_mask, gray
         else:
             return img, class_mask
@@ -105,21 +103,20 @@ class FigaroDataset(Dataset):
         idx = int(filename.strip('Frame').strip('-gt.pbm'))
 
         if 0 < idx <= 150:
-            return 0
-        elif 150 < idx <= 300:
             return 1
-        elif 300 < idx <= 450:
+        elif 150 < idx <= 300:
             return 2
-        elif 450 < idx <= 600:
+        elif 300 < idx <= 450:
             return 3
-        elif 600 < idx <= 750:
+        elif 450 < idx <= 600:
             return 4
-        elif 750 < idx <= 900:
+        elif 600 < idx <= 750:
             return 5
-        elif 900 < idx <= 1050:
+        elif 750 < idx <= 900:
             return 6
+        elif 900 < idx <= 1050:
+            return 7
         raise ValueError
-
 
     @classmethod
     def decode_target(cls, mask):
@@ -127,6 +124,7 @@ class FigaroDataset(Dataset):
         # mask是一个二维数组，其中的每个值对应一个类别标签
         # cmap是一个N x 3的数组，其中N是类别的数目，每一行是对应类别标签的RGB颜色
         return cls.cmap[mask]
+
 
 def download_extract(url, root, filename, md5):
     file_path = os.path.join(root, filename)
@@ -166,9 +164,10 @@ def download_extract(url, root, filename, md5):
     print("Finished!")
 
 
-
 if __name__ == "__main__":
     # Assuming the root directory you want to use is "./data"
     root_dir = "./data"
     # Instantiate the dataset object with download=True to trigger the download
-    dataset = FigaroDataset(root_dir=root_dir, download=True)
+    dataset = FigaroDataset(root_dir=root_dir, download=False)
+    img, mask = dataset.__getitem__(0)
+    print(mask)
